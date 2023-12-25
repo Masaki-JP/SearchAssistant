71eb66e4f7c936bb8cd01db52bf9090bd4b96e33
Masaki Doi <dwarf_s_w@yahoo.co.jp>
2023-12-25 10:26:05 +0900

    docs: SuggestionFetcherのドキュメントを追加

--- active/Search Assistant/Search Assistant/Models/SuggestionFetcher.swift
+++ active/Search Assistant/Search Assistant/Models/SuggestionFetcher.swift
@@ -1,9 +1,18 @@
 import Foundation
 
+/// `SuggestionFetcher` は、ユーザー入力に基づいて提案をフェッチするためのクラスです。
 final class SuggestionFetcher {
+    /// シングルトンインスタンスを提供します。
     static let shared = SuggestionFetcher()
+    /// プライベートイニシャライザは外部からのインスタンス化を防ぎます。
     private init() {}
 
+    /// ユーザー入力に基づいて提案を非同期的にフェッチします。
+    ///
+    /// - Parameters:
+    ///   - userInput: 検索クエリとして使用するユーザー入力文字列。
+    /// - Returns: 検索提案の文字列配列。
+    /// - Throws: ネットワークリクエストに関連するエラー。
     func fetch(from userInput: String) async throws -> [String] {
         let url = createURL(from: userInput)
         let (data, _) = try await URLSession.shared.data(from: url)
@@ -13,6 +22,10 @@ final class SuggestionFetcher {
     }
 }
 
+/// ユーザー入力から検索URLを作成します。
+///
+/// - Parameter userInput: 検索クエリとして使用するユーザー入力文字列。
+/// - Returns: 構築されたURL。
 private func createURL(from userInput: String) -> URL {
     let prefixURL = "https://www.google.com/complete/search?hl=ja&output=toolbar&q="
     let query = userInput.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
@@ -20,6 +33,10 @@ private func createURL(from userInput: String) -> URL {
     return url
 }
 
+/// XML形式の文字列を文字列の配列に変換します。
+///
+/// - Parameter xmlString: 変換するXML形式の文字列。
+/// - Returns: 抽出された提案の文字列配列。
 private func convertXMLStringToArray(xmlString: String) -> [String] {
     var suggestions: [String] = .init()
     let unfinishedXmlElements = xmlString.components(separatedBy: "<CompleteSuggestion><suggestion data=\"")
