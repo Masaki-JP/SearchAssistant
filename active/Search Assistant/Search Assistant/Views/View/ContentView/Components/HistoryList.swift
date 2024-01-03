cfbe872961ac30c62d9b6113b75d65d67828f6b4
Masaki Doi <dwarf_s_w@yahoo.co.jp>
2024-01-03 17:27:31 +0900

    change: HistoryListのリスト項目を修正

--- active/Search Assistant/Search Assistant/Views/View/ContentView/Components/HistoryList.swift
+++ active/Search Assistant/Search Assistant/Views/View/ContentView/Components/HistoryList.swift
@@ -7,26 +7,27 @@ struct HistoryList: View {
         List {
             Section {
                 ForEach(vm.historys) { history in
-                    HStack {
-                        Text(history.platform.iconCharacter)
-                            .font(.title3)
-                            .bold()
-                            .foregroundStyle(.white)
-                            .frame(width: 28, height: 28)
-                            .background(history.platform.imageColor)
-                            .clipShape(RoundedRectangle(cornerRadius: 7))
-                        Button(history.userInput) {
-                            vm.search(history.userInput, on: history.platform)
+                    Button(action: {
+                        vm.search(history.userInput, on: history.platform)
+                    }, label: {
+                        HStack {
+                            Text(history.platform.iconCharacter)
+                                .font(.title3)
+                                .bold()
+                                .foregroundStyle(.white)
+                                .frame(width: 28, height: 28)
+                                .background(history.platform.imageColor)
+                                .clipShape(RoundedRectangle(cornerRadius: 7))
+                            Text(history.userInput)
+                                .padding(.leading, 4)
+                            Spacer()
+                            Text(vm.getDateString(from: history.date))
+                                .font(.caption2)
+                                .foregroundStyle(.secondary)
+                                .padding(.vertical, 4)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
                         }
-                        .foregroundStyle(.primary)
-                        .padding(.leading, 4)
-                        Spacer()
-                        Text(vm.getDateString(from: history.date))
-                            .font(.caption2)
-                            .foregroundStyle(.secondary)
-                            .padding(.vertical, 4)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                    }
+                    })
                 }
                 .onDelete { indexSet in
                     vm.removeHistory(atOffsets: indexSet)
