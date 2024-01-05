6bda002dd076d713f15019d414b29ca3bb2ce557
Masaki Doi <dwarf_s_w@yahoo.co.jp>
2024-01-05 14:05:15 +0900

    add: NoContentsViewの追加

---
+++ active/Search Assistant/Search Assistant/Views/View/ContentView/Components/NoContentsView.swift
@@ -0,0 +1,42 @@
+import SwiftUI
+
+struct NoContentsView: View {
+    let title: String
+    let description: String?
+    let imageSystemName: String
+
+    init(title: String, description: String? = nil, imageSystemName: String) {
+        self.title = title
+        self.description = description
+        self.imageSystemName = imageSystemName
+    }
+
+    var body: some View {
+        VStack(spacing: 5) {
+            Image(systemName: imageSystemName)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 100)
+                .foregroundStyle(.secondary)
+            Text(title)
+                .fontWeight(.bold)
+                .font(.title2)
+
+            if let description {
+                Text(description)
+                    .multilineTextAlignment(.leading)
+                    .foregroundStyle(.secondary)
+                    .font(.footnote)
+            }
+        }
+        .frame(minWidth: 350)
+    }
+}
+
+#Preview {
+    NoContentsView(
+        title: "I am Search Assistant!",
+        description: "Google, Twitter, Instagram, Amazon,  YouTubeなどの\n検索をこのアプリひとつで行うことができます。",
+        imageSystemName: "doc.text.magnifyingglass"
+    )
+}
