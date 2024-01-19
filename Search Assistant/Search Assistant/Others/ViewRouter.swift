135863cca04eda9138686d41d772cb45e960dcfa
Masaki-JP <masakidayo0508@gmail.com>
2024-01-20 07:42:34 +0900

    doc: ViewRouterのドキュメントを追加

--- Search Assistant/Search Assistant/Others/ViewRouter.swift
+++ Search Assistant/Search Assistant/Others/ViewRouter.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+/// - Important: スプラッシュスクリーンの実装に`ViewRouter`を使用せず、overlayモディファイアなどを用いての実装を試みたところシステムイメージなどが読み込まれないなどといった不具合が生じたため、スプラッシュスクリーンは`ViewRouter`を使用する。（2024年1月20日）
 @MainActor
 final class ViewRouter: ObservableObject {
     @Published private(set) var currentView: ViewDestination = .splashScreenView
