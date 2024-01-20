80288aeb3770a6318f4e7f6a2a68c539c4d93deb
Masaki-JP <masakidayo0508@gmail.com>
2024-01-20 09:49:33 +0900

    refactor: ハードコーディングされていたUserDefaultsのキーの対処

--- Search Assistant/Search Assistant/Others/KeyboardToolbarButtonsModel.swift
+++ Search Assistant/Search Assistant/Others/KeyboardToolbarButtonsModel.swift
@@ -1,12 +1,13 @@
 import Foundation
 
 struct KeyboardToolbarButtonsModel {
+    private let userDefaultsKey = "keyboardToolbarButtons"
     var validButtons: Set<SASerchPlatform> = [
         .google, .twitter, .instagram, .amazon, .youtube, .facebook, .mercari, .rakuma, .paypayFleaMarket
     ]
 
     init() {
-        guard let decodedData = UserDefaults.standard.data(forKey: "keyboardToolbarButtons"),
+        guard let decodedData = UserDefaults.standard.data(forKey: userDefaultsKey),
               let keyboardToolbarButtons = try? JSONDecoder().decode(Set<SASerchPlatform>.self, from: decodedData)
         else { return }
         self.validButtons = keyboardToolbarButtons
@@ -20,8 +21,9 @@ struct KeyboardToolbarButtonsModel {
         }
         updateUserDefaults()
     }
+
     private func updateUserDefaults() {
         let encodedData = try! JSONEncoder().encode(validButtons)
-        UserDefaults.standard.set(encodedData, forKey: "keyboardToolbarButtons")
+        UserDefaults.standard.set(encodedData, forKey: userDefaultsKey)
     }
 }
