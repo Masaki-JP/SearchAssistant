0fa762f2c1b44448bab7ae90830c8d9a82bb79ba
Masaki-JP <masakidayo0508@gmail.com>
2024-01-22 09:58:37 +0900

    doc: ContentViewModelのドキュメントを追加

--- Search Assistant/Search Assistant/ViewModels/ContentViewModel/ContentViewModel.swift
+++ Search Assistant/Search Assistant/ViewModels/ContentViewModel/ContentViewModel.swift
@@ -5,11 +5,11 @@ final class ContentViewModel: ContentViewModelProtocol {
     ///
     ///
     ///
-    ///
-    ///
     /// 【Search History】
     ///
+    /// ContentViewModel内部で保持する検索履歴
     @Published private var _historys: [SASerachHistory] = []
+    /// 外部に公開する整形された検索履歴
     var historys: [HistoryInfo] {
         self._historys.map { history in
             HistoryInfo(
@@ -20,24 +20,24 @@ final class ContentViewModel: ContentViewModelProtocol {
             )
         }
     }
-
+    /// ContentViewに提供する検索履歴
     struct HistoryInfo: Identifiable {
         let userInput: String
         let platform: SASerchPlatform
         let dateString: String
         let id: UUID
     }
-
+    /// 検索履歴を保管するためのクラス
     private let historyStore = HistoryStore.shared
-
+    /// 検索履歴を追加する
     private func appendHistory(userInput: String, platform: SASerchPlatform) {
         historyStore.append(userInput: userInput, platform: platform)
     }
-
+    /// 特定の検索履歴を削除する
     func removeHistory(atOffsets indexSet: IndexSet) {
         historyStore.remove(atOffsets: indexSet)
     }
-
+    /// 全ての検索履歴を削除する
     func removeAllHistorys() {
         historyStore.removeAll()
     }
@@ -48,9 +48,11 @@ final class ContentViewModel: ContentViewModelProtocol {
     ///
     /// 【Search Suggestion】
     ///
+    /// 検索候補を保持する変数
     @Published private(set) var suggestions: [String]? = []
+    /// 検索候補を取得するクラス
     private let suggestionFetcher = SuggestionFetcher.shared
-
+    /// ユーザー入力から検索候補を取得する
     func getSuggestion(from userInput: String) async {
         do {
             try await suggestions = suggestionFetcher.fetch(from: userInput)
@@ -65,7 +67,9 @@ final class ContentViewModel: ContentViewModelProtocol {
     ///
     /// 【Search Executer】
     ///
+    /// 検索を行うためのクラス
     var searcher = Searcher()
+    /// ユーザー入力とプラットフォームを元に検索を実行し、履歴に追加する
     func search(_ userInput: String, on platform: SASerchPlatform) {
         do {
             try searcher.Search(userInput, on: platform)
@@ -89,8 +93,11 @@ final class ContentViewModel: ContentViewModelProtocol {
     ///
     /// 【Presentation】
     ///
+    /// SettingViewの表示状態を管理する変数
     @Published var isPresentedSettingView = false
+    /// インスタグラムエラーアラートの表示状態を管理する変数
     @Published var isShowInstagramErrorAlert = false
+    /// 全履歴削除の確認を行うプロンプトの表示状態を管理する変数
     @Published var isShowPromptToConfirmDeletionOFAllHistorys = false
     ///
     ///
@@ -99,8 +106,10 @@ final class ContentViewModel: ContentViewModelProtocol {
     ///
     /// 【Settings】
     ///
+    /// キーボードの自動表示の設定を保持する変数
     @AppStorage(AppStorageKey.autoFocus)
     private(set) var settingAutoFocus = true
+    /// 検索ボタンの位置の設定を保持する変数
     @AppStorage(AppStorageKey.searchButton_Left)
     private(set) var settingLeftSearchButton = false
     ///
@@ -110,8 +119,11 @@ final class ContentViewModel: ContentViewModelProtocol {
     ///
     /// 【Setting: KeyboardToolbarValidButton】
     ///
+    /// 有効化されているキーボードツールバーボタンを保持する変数
     @Published private(set) var keyboardToolbarValidButtons: Set<SASerchPlatform>
+    /// キーボードツールバーボタンの有効無効を管理するクラス
     private let keyboardToolbarValidButtonManager = UserDefaultsRepository<Set<SASerchPlatform>>(key: UserDefaultsKey.keyboardToolbarValidButtons)
+    /// 有効化されているキーボードツールバーボタンを取得する
     func fetchKeyboardToolbarValidButtons() {
         do {
             keyboardToolbarValidButtons = try keyboardToolbarValidButtonManager.fetch()
@@ -127,24 +139,32 @@ final class ContentViewModel: ContentViewModelProtocol {
     ///
     /// 【Others】
     ///
+    /// テキストフィールドで用いるテキストを保持する変数
     @Published var userInput = ""
+    /// 日付フォーマットが"yyyy/MM/dd"で設定されているDateFormatter
     private let dateFormatter: DateFormatter
+    ///
+    ///
     ///
     ///
     ///
     /// 【Initializer】
     ///
     init() {
+        /// 内部で使用するdateFormatter設定する
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy/MM/dd"
         dateFormatter.calendar = Calendar.autoupdatingCurrent
         self.dateFormatter = dateFormatter
+        /// 保存されている有効化されているキーボードツールバーボタンを取得する
+        /// 失敗時には全てのキーボードツールバーボタンを有効化する
         do {
             keyboardToolbarValidButtons = try keyboardToolbarValidButtonManager.fetch()
         } catch {
             reportError(error)
             keyboardToolbarValidButtons = Set(SASerchPlatform.allCases)
         }
+        /// historyStoreが保持するhistorysの変更を内部の_historys変数に伝播させる
         historyStore.$historys
             .receive(on: DispatchQueue.main)
             .assign(to: &self.$_historys)
