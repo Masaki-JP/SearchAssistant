6875c5e738eebb04bf8fee73ba38a4166508cfb1
Masaki-JP <masakidayo0508@gmail.com>
2026-06-06 01:32:17 +0900

    change: DebugLogを分離

--- Search Assistant/Search Assistant/Views/Search_AssistantApp.swift
+++ Search Assistant/Search Assistant/Views/Search_AssistantApp.swift
@@ -26,30 +26,3 @@ struct Search_AssistantApp: App {
         }
     }
 }
-
-func reportError(
-    fileID: String = #fileID,
-    function: String = #function,
-    line: Int = #line,
-    _ error: Error
-) {
-    print("💥💥💥")
-    print("fileID: \(fileID)")
-    print("function: \(function)")
-    print("line: \(line)")
-    print("error: \(error) (\(type(of: error)))")
-    print("💥💥💥")
-}
-
-func reportMockAction(
-    fileID: String = #fileID,
-    function: String = #function,
-    line: Int = #line
-) {
-    print("")
-    print("Called MockAction")
-    print("function: \(function)")
-    print("line: \(line)")
-    print("fileID: \(fileID)")
-    print("")
-}
