7807087627eb0844b34e584dca2199f5365ac2cc
Masaki-JP <masakidayo0508@gmail.com>
2026-06-17 18:38:27 +0900

    change: rawValueの用途コメントを追加

--- SearchAssistant/Others/AppStorageKey.swift
+++ SearchAssistant/Others/AppStorageKey.swift
@@ -1,6 +1,8 @@
 import Foundation
 
 extension UserDefaultsKey {
+    /// rawValue は @AppStorage のキー文字列として使用される。
+    /// 
     enum AppStorageKey: String {
         case autoFocus
         case searchButtonLeft
