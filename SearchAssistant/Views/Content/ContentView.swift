cd847ce19d01649c35689e3baf95e7e3f20b74e4
Masaki-JP <masakidayo0508@gmail.com>
2026-08-16 18:17:01 +0900

    SettingsViewにナビゲーションタイトルを設定

--- SearchAssistant/Views/Content/ContentView.swift
+++ SearchAssistant/Views/Content/ContentView.swift
@@ -125,7 +125,6 @@ struct ContentView<BookmarkRepositoryType: BookmarkRepositoryProtocol, EnabledSe
                 bookmarkRepository: bookmarkRepository,
                 enabledSearchButtonRepository: enabledSearchButtonRepository
             )
-            .inlineNavigationTitle("各種設定")
             .preferredColorScheme(colorScheme)
         }
         .alert("確認", isPresented: $isPresentedDeleteAllHistoriesAlert) {
